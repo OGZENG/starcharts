d23cf8903903bcd65c00766296ca1c26fb1a51df
YuxuanZeng <yuxuan.zeng@tum.de>
2025-05-13 20:24:55 +0200

    new code review

--- main.go
+++ main.go
@@ -21,41 +21,42 @@ import (
 )
 
 //go:embed static/*
-var static embed.FS //编译期间将文件或整个目录打包进Go程序里
+var static embed.FS //编译期间将文件或整个目录打包进Go程序里 file system
 
 var version = "devel" //development 开发中的版本
 
 func main() {
-	log.SetHandler(text.New(os.Stderr))
+	log.SetHandler(text.New(os.Stderr)) //日志处理器 纯文本格式的日志 日志就是程序运行时发生的事情的记录
 	// log.SetLevel(log.DebugLevel)
-	config := config.Get()
-	ctx := log.WithField("listen", config.Listen)
-	options, err := redis.ParseURL(config.RedisURL)
+	config := config.Get()                          //从项目自定义的config包中获取配置
+	ctx := log.WithField("listen", config.Listen)   //设置一个带上下文信息的日志对象
+	options, err := redis.ParseURL(config.RedisURL) //解析Redis URL返回连接参数
 	if err != nil {
 		log.WithError(err).Fatal("invalid redis_url")
 	}
-	redis := redis.NewClient(options)
-	cache := cache.New(redis)
-	defer cache.Close()
-	github := github.New(config, cache)
+	redis := redis.NewClient(options) /*创建一个新的Redis客户端实例 Redis就是一个运行在内存中的超快的数据库，
+	类似于费大厨回锅肉这道菜会专门放出来*/
+	cache := cache.New(redis)           //通过redis客户端创建一个缓存系统
+	defer cache.Close()                 //才学的defer函数，当main结束后延迟执行close
+	github := github.New(config, cache) //创建一个Github客户端或服务
 
-	r := mux.NewRouter()
+	r := mux.NewRouter() //创建一个新的路由器对象r，用它来注册所有URL和它们对应的处理函数
+	r.Path("/").         //访问主页 get，处理方式是调用controller的index函数
+				Methods(http.MethodGet).
+				Handler(controller.Index(static, version))
 	r.Path("/").
-		Methods(http.MethodGet).
-		Handler(controller.Index(static, version))
-	r.Path("/").
-		Methods(http.MethodPost).
+		Methods(http.MethodPost). //提交表单时浏览器会发起一个post请求，由HandleForm处理
 		HandlerFunc(controller.HandleForm())
 	r.PathPrefix("/static/").
-		Methods(http.MethodGet).
-		Handler(http.FileServer(http.FS(static)))
-	r.Path("/{owner}/{repo}.svg").
-		Methods(http.MethodGet).
-		Handler(controller.GetRepoChart(github, cache))
-	r.Path("/{owner}/{repo}").
-		Methods(http.MethodGet).
-		Handler(controller.GetRepo(static, github, cache, version))
-
+		Methods(http.MethodGet).                  //当用户请求资源时会从嵌入的static目录中读取文件
+		Handler(http.FileServer(http.FS(static))) //用http.FileServer把嵌入的文件作为HTTP服务器的静态文件目录
+	r.Path("/{owner}/{repo}.svg"). //返回SVG格式图表
+					Methods(http.MethodGet).
+					Handler(controller.GetRepoChart(github, cache))
+	r.Path("/{owner}/{repo}"). //请求仓库信息页面
+					Methods(http.MethodGet).
+					Handler(controller.GetRepo(static, github, cache, version))
+	//定义谁来处理什么
 	// generic metrics
 	requestCounter := promauto.NewCounterVec(prometheus.CounterOpts{
 		Namespace: "starcharts",
